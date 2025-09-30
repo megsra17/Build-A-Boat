4dbd5c69aa3a23949a8789e23a42e739bdf5b3b8
Megan <megsra17@gmail.com>
2025-09-30 16:08:31 -0500

    chore: Update AssemblyInformationalVersion and cache inputs for consistency

--- Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fa2c80cbda3e7eade52541cf51dce92c5b55bc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84a9e097a17917b7815061708f1c78521d6d2a4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
